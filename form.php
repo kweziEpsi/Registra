3a7fbe20d542171b9eab6f08c82e0f14f4c37058
kweziEpsi <kwezi@digilims.com>
2021-10-05 10:56:34 +0200

    Fixed (\n) new line bug

--- form.php
+++ form.php
@@ -3,8 +3,8 @@
 
     /*Recieve data from form and write on txt file*/
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
+    $name = $_POST["name"]." ";
+    $email =$_POST["email"]."\n";
     $register = fopen("register.txt", "a")or die("Unable to open file!"); //create .txt file
     fwrite($register, $name); //write name to .txt file 
     fwrite($register, $email); //write email to .txt
