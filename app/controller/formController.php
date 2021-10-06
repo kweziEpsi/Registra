e5d37564fc46916c1d297c942a2e67ab7f026801
kweziEpsi <kwezi@digilims.com>
2021-10-06 12:27:07 +0200

    Added comments to code

--- app/controller/formController.php
+++ app/controller/formController.php
@@ -3,11 +3,13 @@
     include('../../app/model/form.php');
     
     $name = $email = ""; //varaibles
-    /*Recieve data from form and write on txt file*/
+
+    /*Start recieve data*/
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $name = $_POST["name"]." ";
         $email =$_POST["email"]."\n";
-        write($name,$email);
-        header('Location: ../../app/view/redirect.html'); //redirect to diffent page
+        write($name,$email); //submit data to form model
+        header('Location: ../../app/view/redirect.html'); //redirect to thank you page
     }
+    /*End recieve data*/
 ?>
