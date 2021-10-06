1c7eb209234207e0d5f74529e48d4c69f80e79db
kweziEpsi <kwezi@digilims.com>
2021-10-06 12:01:45 +0200

    Updated final mvc structure and code

--- app/controller/formController.php
+++ app/controller/formController.php
@@ -1,12 +1,13 @@
 <?php
-    include 'form.php';
     
-
+    include('../../app/model/form.php');
+    
     $name = $email = ""; //varaibles
     /*Recieve data from form and write on txt file*/
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $name = $_POST["name"]." ";
         $email =$_POST["email"]."\n";
         write($name,$email);
+        header('Location: ../../app/view/redirect.html'); //redirect to diffent page
     }
 ?>
--- app/model/form.php
+++ app/model/form.php
@@ -1,7 +1,6 @@
 <?php
-    header('Location: redirect.html'); //redirect to diffent page
     function write($name,$email){
-        $register = fopen("register.txt", "a")or die("Unable to open file!"); //create .txt file
+        $register = fopen("../../app/view/register.txt", "a")or die("Unable to open file!"); //create .txt file
         fwrite($register, $name); //write name to .txt file 
         fwrite($register, $email); //write email to .txt
         fclose($register);
