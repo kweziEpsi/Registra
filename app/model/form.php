2bce13925d68731aa22ecb67e345742b9e5d8fe3
kweziEpsi <kwezi@digilims.com>
2021-10-06 12:32:32 +0200

    Changed path for register.txt file

--- app/model/form.php
+++ app/model/form.php
@@ -1,9 +1,9 @@
 <?php
     function write($name,$email){
-        $register = fopen("../../app/view/register.txt", "a")or die("Unable to open file!"); //create .txt file
+        $register = fopen("register.txt", "a")or die("Unable to open file!"); //create .txt file
         fwrite($register, $name); //write name to .txt file 
         fwrite($register, $email); //write email to .txt
-        fclose($register);
+        fclose($register); 
     }
 
 ?>
