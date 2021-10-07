84f96a64e60eab095b38cf9d65b425e60e67422d
kweziEpsi <kwezi@digilims.com>
2021-10-07 11:55:40 +0200

    Final commit

--- app/model/form.php
+++ app/model/form.php
@@ -2,8 +2,8 @@
     function write($name,$email){
         $register = fopen("register.txt", "a")or die("Unable to open file!"); //create .txt file
         fwrite($register, $name); //write name to .txt file 
-        fwrite($register, $email); //write email to .txt
-        fclose($register); 
+        fwrite($register, $email); //write email to .txt file
+        fclose($register); //close .txt file 
     }
 
 ?>
